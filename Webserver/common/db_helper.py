5ad631ae180fa2efbc254e52a109765c23dfcbbc
rrrral <295536022@qq.com>
2018-09-22 11:07:22 +0800

    Some functions have been added. Unfinished

--- Webserver/common/db_helper.py
+++ Webserver/common/db_helper.py
@@ -1,7 +1,24 @@
-# -*- coding: UTF-8 -*-
+#!/usr/bin/evn python
+# coding=utf-8
 
 import pymysql
 import re
+
+from Webserver.config import const
+
+
+#  查找是否具有某数据库
+def database_exists(cursor, db_name):
+    sql = "show databases;"
+    cursor.execute(sql)
+    tables = [cursor.fetchall()]
+    table_list = re.findall('(\'.*?\')', str(tables))
+    table_list = [re.sub("'", '', each) for each in table_list]
+    if db_name in table_list:
+        # 存在返回1
+        return 1
+    else:
+        return 0
 
 
 #  这个函数用来判断表是否存在
@@ -9,7 +26,7 @@ def table_exists(cursor, table_name):
     sql = "show tables;"
     cursor.execute(sql)
     tables = [cursor.fetchall()]
-    table_list = re.findall('(\'.*?\')',str(tables))
+    table_list = re.findall('(\'.*?\')', str(tables))
     table_list = [re.sub("'", '', each) for each in table_list]
     if table_name in table_list:
         # 存在返回1
@@ -18,23 +35,37 @@ def table_exists(cursor, table_name):
         return 0
 
 
+#  创建新的库
+def create_database(cursor, db_name):
+    # 首先检测此数据库是否存在
+    if database_exists(cursor, db_name) is 0:
+        sql_create = "create database {}".format(db_name)
+        cursor.execute(sql_create)
+
+
+#  删除某数据库
+def del_database(cursor, db_name):
+    sql_create = "drop database if exists {}".format(db_name)
+    cursor.execute(sql_create)
+
+
 # 提供账户及密码登陆数据库
 def login_db(user, pwd, db):
-    db = pymysql.connect(host='127.0.0.1', port=3306, user=user, passwd=pwd, db=db, charset="utf8")
+    db = pymysql.connect(host=const.DB_HOST, port=const.DB_PORT, user=user, passwd=pwd, db=db, charset="utf8")
     # 获取游标对象
     cursor = db.cursor()
-    return {db, cursor}
+    return [db, cursor]
 
 
-# 创建表user
+# 创建表student
 def create_user_form(cursor):
-    sql_create = "create table user(id int, name varchar(18), pwd varchar(18)) engine = innodb charset = utf8"
+    sql_create = "create table student(id int, name varchar(18), pwd varchar(18)) engine = innodb charset = utf8"
     cursor.execute(sql_create)
 
 
 # 加入新用户
 def insert_user(db, cursor, user_id, name, pwd):
-    sql_insert = '''insert into user values ("{}", "{}", "{}")'''.format(user_id, name, pwd)
+    sql_insert = '''insert into student values ("{}", "{}", "{}")'''.format(user_id, name, pwd)
     try:
         cursor.execute(sql_insert)
         db.commit()
--- Webserver/common/mail_sent.py
+++ Webserver/common/mail_sent.py
@@ -1,4 +1,5 @@
-# -*- coding: utf-8 -*-
+#!/usr/bin/evn python
+# coding=utf-8
 
 import smtplib
 from email.mime.text import MIMEText
@@ -15,8 +16,8 @@ def mail(sender_nickname, recipient_nickname, text_content):
         msg['To'] = formataddr([recipient_nickname, const.EMAIL_LIST])  # 括号里的对应收件人邮箱昵称、收件人邮箱账号
         msg['Subject'] = const.EMAIL_ERR_TITLE  # 邮件的主题，也可以说是标题
 
-        # 发件人邮箱中的SMTP服务器，端口是465
-        server = smtplib.SMTP_SSL("smtp.qq.com", 465)
+        # 发件人邮箱中的SMTP服务器
+        server = smtplib.SMTP_SSL(const.EMAIL_SMTP, const.DB_HOST)
         # 括号中对应的是发件人邮箱账号、邮箱密码
         server.login(const.EMAIL_USER, const.EMAIL_PWD)
         # 括号中对应的是发件人邮箱账号、收件人邮箱账号、发送邮件
