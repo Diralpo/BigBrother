3955de226c7b93c2c97031a42d670fa6957cac23
rrrral <295536022@qq.com>
2018-09-25 23:50:58 +0800

    add readme

---
+++ README.md
@@ -0,0 +1,4 @@
+# 简易学生信息系统 #
+
+运行前需要先配置 Webserver/config/const.py 中的变量
+需在本地建立相应的数据库，或直接运行给出的 db_test.py 
--- Webserver/config/const.py
+++ Webserver/config/const.py
@@ -5,15 +5,15 @@
 ##################################################################################
 ### 数据库参数 ###
 # 数据库名称
-DB_NAME = 'studentInfo'
+DB_NAME = 'studentinfo'
 # 数据库链接地址
 DB_HOST = '127.0.0.1'
 # 数据库端口
 DB_PORT = 3306
 # 数据库账号
-DB_USER = 'root'
+DB_USER = 'xxxxxx'
 # 数据库登录密码
-DB_PASS = '123456'
+DB_PASS = '**************'
 
 ##################################################################################
 ### 邮件服务参数 ###
@@ -22,10 +22,10 @@ EMAIL_SMTP = 'smtp.qq.com'
 # 邮件服务器端口
 EMAIL_PORT = 465
 # email发送账号
-EMAIL_USER = '735612944@qq.com'
+EMAIL_USER = 'xxxxxx@qq.com'
 # email发送密码
-EMAIL_PWD = 'mjrontqyobydbfad'
+EMAIL_PWD = '************'
 # 系统异常邮件通知地址
-EMAIL_LIST = '295536022@qq.com'
+EMAIL_LIST = 'xxxxx@qq.com'
 # 异常邮件通知
 EMAIL_ERR_TITLE = '系统异常通知'
